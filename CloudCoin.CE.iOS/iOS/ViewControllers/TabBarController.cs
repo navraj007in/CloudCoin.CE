c939a8a56a64f91e052ff779c07d040b96353983
Navraj Singh <navraj24031981@gmail.com>
2017-09-29 22:02:09 +0530

    Xamarin.iOS Project Initialised

---
+++ CloudCoin.CE.iOS/iOS/ViewControllers/TabBarController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UIKit;
+
+namespace CloudCoin.CE.iOS.iOS
+{
+    public partial class TabBarController : UITabBarController
+    {
+        public TabBarController(IntPtr handle) : base(handle)
+        {
+            TabBar.Items[0].Title = "Browse";
+            TabBar.Items[1].Title = "About";
+        }
+    }
+}
